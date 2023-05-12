9c8f6997972f13afe9be3dbdc695a12cc6f144c8
vikyvilla <vigneshvilha@gmail.com>
2023-05-12 21:31:40 +0530

    Dummy commit

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,4 +1,13 @@
+
+async function dummy() {
+  for (let i=0;i<300;i++) {
+    const p1 =  new Promise((res) => setTimeout(() => res("p" + i), 3000));
+    console.log(await p1);
+  }
+}
+
 function main(args) {
+    dummy();
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
